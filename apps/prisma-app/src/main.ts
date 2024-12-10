d8e126f12cf3959007e48baed8cefedadec5c0a3
Susumu Fujii <powermac.g7@gmail.com>
2024-12-10 20:12:12 +0900

    feat: modify graphql-build sequence

--- apps/prisma-app/src/main.ts
+++ apps/prisma-app/src/main.ts
@@ -1,9 +1,8 @@
 import "reflect-metadata";
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
-import { schema } from "schema";
-import { prisma } from "context";
-
+import { schema } from "./scheme"
+import { prisma } from "./context"
 const app = express();
 app.use("/graphql", graphqlHTTP(async (request) => ({
   schema,
